786847d186042bb5ae37f1416287ccea0c9da814
bd_ <bd_@nadena.dev>
2024-04-27 16:39:16 -0700

    feat: functional prototype

---
+++ Core/ReactiveQuery/SynchronousTaskScheduler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Core.ReactiveQuery
+{
+    internal sealed class SynchronousTaskScheduler : TaskScheduler
+    {
+        internal static SynchronousTaskScheduler Instance { get; } = new SynchronousTaskScheduler();
+        
+        protected override IEnumerable<Task> GetScheduledTasks()
+        {
+            return Array.Empty<Task>();
+        }
+
+        protected override void QueueTask(Task task)
+        {
+            base.TryExecuteTask(task);
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            base.TryExecuteTask(task);
+            return true;
+        }
+    }
+}
