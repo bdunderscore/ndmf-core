dca985ace145105e36de6e9982e5e850ace286b2
bd_ <bd_@nadena.dev>
2024-04-27 17:54:51 -0700

    chore(docs): add docs

--- Core/ReactiveQuery/ReactiveQuery.cs
+++ Core/ReactiveQuery/ReactiveQuery.cs
@@ -7,19 +7,66 @@ using JetBrains.Annotations;
 
 namespace nadena.dev.ndmf.ReactiveQuery
 {
+    /// <summary>
+    /// A ReactiveQuery represents a cached computation, which can be automatically invalidated and recomputed when
+    /// something it relied upon changes.
+    ///
+    /// ## Obtaining values from a ReactiveQuery
+    ///
+    /// There are four ways to obtain a value from a ReactiveQuery:
+    /// 1. Continually, by subscribing to the query using `Subscribe`. This option will trigger the computation of the
+    ///    value if it is not already available, and will also trigger the computation of the value whenever the query is
+    ///    invalidated.
+    /// 2. Speculatively, using `TryGetValue` (which will return `false` if the value is not yet available). This option
+    ///    does not trigger the computation of the value in most cases.
+    /// 2. Asynchronously, using `GetValueAsync` (which will return a `Task<T>` that will complete when the value is
+    ///    available). This option will trigger the computation of the value if it is not already available.
+    /// 4. From within another ReactiveQuery, by using `ComputeContext.Observe`. This will record the dependency on the
+    ///    other query, and will trigger the computation of the value if it is not already available. It will then
+    ///    arrange for the calling query to be re-computed whenever the observed query is invalidated.
+    ///
+    /// ## Writing a ReactiveQuery
+    ///
+    /// To write a ReactiveQuery, you must implement the `Compute` and `ToString` methods. `Compute` will be invoked
+    /// when the query is invalidated, and should return the new value of the computation. Note that, if something
+    /// is invalidated while the query is updating, the computation will be cancelled (throwing `TaskCancelledException`s
+    /// when observing sub-queries) and restarted.
+    ///
+    /// Generally, it is important to cache the ReactiveQuery itself somehow (you won't get a lot of benefit from caching
+    /// if you create a new ReactiveQuery every time!)
+    ///
+    /// ## Threading notes
+    ///
+    /// ReactiveQuery will avoid invoking Compute more than once in parallel; even if invalidated, the last execution
+    /// will run to completion (and its result will be ignored), then the new computation will be started.
+    ///
+    /// Likewise, when using the IObservable interface to observe the state of a query, ReactiveQuery will not invoke
+    /// methods on any particular observer in parallel. It is not guaranteed that observers will see every computed
+    /// value; we only guarantee that eventually, if invalidations stop, all observers will see the last value.
+    ///
+    /// ## Using ReactiveQuery in Unity
+    ///
+    /// When using RQ in Unity, generally you'll want to use the `Subscribe` method to drive various unity editor UI
+    /// bits. If you need to access a ReactiveQuery _synchronously_ (for example, in a NDMF plugin), refer to
+    /// `ReactiveQueryUnityExt.GetSync`.
+    ///
+    /// The default TaskFactory for RQ on Unity projects will be one that runs on the Unity main thread. You can
+    /// override this by overriding the `Scheduler` property with an appropriate `TaskFactory`.
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public abstract class ReactiveQuery<T> : IObservable<T>
     {
-        protected TaskFactory Scheduler { get; } = Task.Factory;
+        // TODO: Override to unity main thread scheduler
+        protected virtual TaskFactory Scheduler { get; } = Task.Factory;
         
         #region State
         
-        private object _lock = new object();
+        private object _lock = new();
         
         // Locked by _lock
         private long _invalidationCount = 0;
         
-        [PublicAPI]
-
         private CancellationToken _cancellationToken = CancellationToken.None;
         private Task _cancelledTask = null;
         private TaskCompletionSource<object> _invalidated = null;
@@ -34,6 +81,47 @@ namespace nadena.dev.ndmf.ReactiveQuery
         
         #region Public API
 
+        private class SimpleQuery<T> : ReactiveQuery<T>
+        {
+            private string _description;
+            private Func<ComputeContext, Task<T>> _compute;
+
+            public SimpleQuery(string description, Func<ComputeContext, Task<T>> compute)
+            {
+                _description = description;
+                _compute = compute;
+            }
+
+            protected override Task<T> Compute(ComputeContext context)
+            {
+                return _compute(context);
+            }
+
+            public override string ToString()
+            {
+                return _description;
+            }
+        }
+        
+        /// <summary>
+        /// Creates a ReactiveQuery based on a computation delegate.
+        /// </summary>
+        /// <param name="description">The description of the query, used in error messages</param>
+        /// <param name="compute">The function to invoke to compute the query</param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static ReactiveQuery<T> Create<T>(string description, Func<ComputeContext, Task<T>> compute)
+        {
+            return new SimpleQuery<T>(description, compute);
+        }
+        
+        /// <summary>
+        /// Attempts to get the current value, but only if it is available immediately. Does not trigger computation if
+        /// the value is not available.
+        /// </summary>
+        /// <param name="value">The value, if available</param>
+        /// <returns>True if the value was available, false if not</returns>
+        /// <exception cref="Exception">If the last computation failed</exception>
         public bool TryGetValue(out T value)
         {
             lock (_lock)
@@ -51,6 +139,10 @@ namespace nadena.dev.ndmf.ReactiveQuery
             }
         }
 
+        /// <summary>
+        /// Returns a Task which will resolve to the query's latest value.
+        /// </summary>
+        /// <returns></returns>
         public async Task<T> GetValueAsync()
         {
             while (true)
@@ -114,11 +206,25 @@ namespace nadena.dev.ndmf.ReactiveQuery
         
         private HashSet<ObserverContext<T>> _observers = new(new ObjectIdentityComparer<ObserverContext<T>>());
 
+        /// <summary>
+        /// Subscribes an observer to this query. The observer will be executed on the TaskScheduler associated with
+        /// this query, or if that is not possible, the default task scheduler for ReactiveQueries.
+        /// </summary>
+        /// <param name="observer"></param>
+        /// <returns>A disposable which will deregister the observer, once disposed</returns>
         public IDisposable Subscribe(IObserver<T> observer)
         {
             return Subscribe(observer, null);
         }
         
+        /// <summary>
+        /// Subscribes an observer to this query. The observer will be executed on the provided TaskScheduler.
+        /// If the provided scheduler is null, the observer will be executed on the TaskScheduler associated with this
+        /// query, or if that is not possible, the default task scheduler for ReactiveQueries.
+        /// </summary>
+        /// <param name="observer"></param>
+        /// <param name="scheduler"></param>
+        /// <returns></returns>
         [PublicAPI]
         public IDisposable Subscribe(IObserver<T> observer, TaskScheduler scheduler)
         {
@@ -177,6 +283,10 @@ namespace nadena.dev.ndmf.ReactiveQuery
             }
         }
 
+        /// <summary>
+        /// Immediately invalidates the query. If there are downstream computations or observers, the query will be
+        /// recomputed.
+        /// </summary>
         public void Invalidate()
         {
             Invalidate(-1);
@@ -186,8 +296,18 @@ namespace nadena.dev.ndmf.ReactiveQuery
         
         #region Subclass API
         
+        /// <summary>
+        /// Invoked when the query needs to be recomputed. This method should return the new value of the computation.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
         protected abstract Task<T> Compute(ComputeContext context);
 
+        /// <summary>
+        /// Invoked when the query is invalidated and the current value is no longer needed. This method should clean up
+        /// any resources associated with the passed value.
+        /// </summary>
+        /// <param name="value"></param>
         protected virtual void DestroyObsoleteValue(T value)
         {
             // no-op
