dbf24c7472df433fc69e4f522b9a7f8e352796f9
bd_ <bd_@nadena.dev>
2024-04-27 17:26:04 -0700

    feat: fixing bugs in Observable handling

--- UnitTests~/BasicQueryTest.cs
+++ UnitTests~/BasicQueryTest.cs
@@ -1,47 +1,15 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
-using nadena.dev.ndmf.ReactiveQuery;
 using NUnit.Framework;
 
 namespace UnitTests
 {
-    class TestQuery : ReactiveQuery<int>
-    {
-        private Func<ComputeContext, Task<int>> _value;
-
-        public Func<ComputeContext, Task<int>> Value
-        {
-            get => _value;
-            set
-            {
-                _value = value;
-                Invalidate();
-            }
-        }
-        
-        public TestQuery(Func<ComputeContext, Task<int>> func)
-        {
-            _value = func;
-        }
-        
-        protected override async Task<int> Compute(ComputeContext context)
-        {
-            return await _value(context);
-        }
-
-        public override string ToString()
-        {
-            return "TestQuery";
-        }
-    }
-    
     public class BasicQueryTest
     {
         [Test]
         public void TrivialQuery()
         {
-            var q = new TestQuery(_ => Task.FromResult(42));
+            var q = new TestQuery<int>(_ => Task.FromResult(42));
 
             Assert.IsFalse(q.TryGetValue(out var _));
             var task = q.GetValueAsync();
@@ -58,7 +26,7 @@ namespace UnitTests
         {
             int value = 1;
             
-            var q = new TestQuery(_ => Task.FromResult(value));
+            var q = new TestQuery<int>(_ => Task.FromResult(value));
 
             Assert.AreEqual(1, await q.GetValueAsync());
             value = 2;
@@ -71,8 +39,8 @@ namespace UnitTests
         {
             int value = 1;
             
-            var q1 = new TestQuery(_ => Task.FromResult(value));
-            var q2 = new TestQuery(async ctx => await ctx.Observe(q1));
+            var q1 = new TestQuery<int>(_ => Task.FromResult(value));
+            var q2 = new TestQuery<int>(async ctx => await ctx.Observe(q1));
             
             Assert.AreEqual(1, await q2.GetValueAsync());
 
@@ -86,8 +54,8 @@ namespace UnitTests
         public void TaskDelay()
         {
             TaskCompletionSource<int> tcs = new TaskCompletionSource<int>();
-            var q = new TestQuery(_ => tcs.Task);
-            var q2 = new TestQuery(async ctx => await ctx.Observe(q));
+            var q = new TestQuery<int>(_ => tcs.Task);
+            var q2 = new TestQuery<int>(async ctx => await ctx.Observe(q));
 
             var t2 = q2.GetValueAsync();
             Task.WaitAll(new Task[] {t2}, 100);
@@ -103,7 +71,7 @@ namespace UnitTests
         public void CancellationAwaitsPreviousExecution()
         {
             TaskCompletionSource<int> tcs = new TaskCompletionSource<int>();
-            var q = new TestQuery(_ => tcs.Task);
+            var q = new TestQuery<int>(_ => tcs.Task);
 
             var task = q.GetValueAsync();
             Task.WaitAll(new Task[] {task}, 100);
@@ -118,6 +86,51 @@ namespace UnitTests
             Task.WaitAll(new Task[] {task2}, 100);
             Assert.IsTrue(task2.IsCompleted);
             Assert.AreEqual(42, task2.Result);
+        }
+
+        [Test]
+        public void ObserveMultipleQueries()
+        {
+            var q1 = new TestQuery<int>(_ => Task.FromResult(1));
+            var q2 = new TestQuery<int>(_ => Task.FromResult(2));
+            var q3 = new TestQuery<int>(async ctx => await ctx.Observe(q1) + await ctx.Observe(q2));
+            
+            Assert.AreEqual(3, q3.GetValueAsync().Result);
+            
+            q2.Value = _ => Task.FromResult(30);
+            Assert.AreEqual(31, q3.GetValueAsync().Result);
+            
+            q1.Value = _ => Task.FromResult(10);
+            Assert.AreEqual(40, q3.GetValueAsync().Result);
+        }
+
+        [Test]
+        public void StopObserving()
+        {
+            var counter = 1;
+            var q1 = new TestQuery<int>(_ => Task.FromResult(counter++));
+
+            var shouldCheck = new TestQuery<bool>(_ => Task.FromResult(true));
+            var q2 = new TestQuery<int>(async ctx =>
+            {
+                var check = await ctx.Observe(shouldCheck);
+                if (check)
+                {
+                    return await ctx.Observe(q1);
+                }
+                else
+                {
+                    return 0;
+                }
+            });
+            
+            Assert.AreEqual(1, q2.GetValueAsync().Result);
+            q1.Invalidate();
+            Assert.AreEqual(2, q2.GetValueAsync().Result);
+            shouldCheck.Value = _ => Task.FromResult(false);
+            Assert.AreEqual(0, q2.GetValueAsync().Result);
+            q1.Invalidate();
+            Assert.AreEqual(0, q2.GetValueAsync().Result);
         }
     }
 }
---
+++ UnitTests~/ObservableInterfaceTest.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class ObservableInterfaceTest
+    {
+        class TestObserver<T> : IObserver<T>
+        {
+            public volatile TaskCompletionSource<object> TaskCompletionSource = new TaskCompletionSource<object>();
+            public List<object> Sequence = new List<object>();
+
+            public void OnCompleted()
+            {
+                if (TaskCompletionSource?.Task.IsCompleted != true) TaskCompletionSource?.SetResult(null);
+                Sequence.Add(null);
+            }
+
+            public void OnError(Exception error)
+            {
+                if (TaskCompletionSource?.Task.IsCompleted != true) TaskCompletionSource?.SetResult(null);
+                Sequence.Add(error);
+            }
+
+            public void OnNext(T value)
+            {
+                if (TaskCompletionSource?.Task.IsCompleted != true) TaskCompletionSource?.SetResult(null);
+                Sequence.Add(value);
+            }
+        }
+        
+        [Test]
+        public void ObservableBasicTest()
+        {
+            var rq = new TestQuery<int>(_ => Task.FromResult(42));
+            var observer = new TestObserver<int>();
+            observer.TaskCompletionSource = new TaskCompletionSource<object>();
+            
+            var remover = rq.Subscribe(observer);
+            Task.WaitAll(new Task[] { observer.TaskCompletionSource.Task }, 100);
+            Assert.AreEqual(new List<object>() { 42 }, observer.Sequence);
+            
+            observer.TaskCompletionSource = new TaskCompletionSource<object>();
+            rq.Value = _ => Task.FromResult(43);
+            Task.WaitAll(new Task[] { observer.TaskCompletionSource.Task }, 100);
+            Assert.AreEqual(new List<object>() { 42, 43 }, observer.Sequence);
+
+            remover.Dispose();
+            
+            observer.TaskCompletionSource = new TaskCompletionSource<object>();
+            rq.Value = _ => Task.FromResult(44);
+            Task.WaitAll(new Task[] { observer.TaskCompletionSource.Task }, 100);
+            Assert.AreEqual(new List<object>() { 42, 43, null }, observer.Sequence);
+        }
+
+        class BarrierObserver : IObserver<int>
+        {
+            public volatile int ObservationCount = 0;
+            public volatile bool Broken = false;
+            public Barrier Barrier = new Barrier(2);
+
+            private int _isActive;
+            private int _index;
+            
+            public BarrierObserver(int index)
+            {
+                _isActive = 0;
+                _index = index;
+            }
+            
+            public void OnCompleted()
+            {
+            }
+
+            public void OnError(Exception error)
+            {
+            }
+
+            public void OnNext(int value)
+            {
+                System.Console.Error.WriteLine($"Observer {_index} got value {value}");
+                Interlocked.Increment(ref ObservationCount);
+                
+                var wasActive = Interlocked.Exchange(ref _isActive, 1);
+                if (wasActive == 1)
+                {
+                    Broken = true;
+                    Barrier.Dispose();
+                }
+                
+                Barrier.SignalAndWait();
+                Barrier.SignalAndWait();
+
+                _isActive = 0;
+            }
+        }
+        
+        [Test]
+        public void LimitsParallelismInObserverScope()
+        {
+            Barrier barrier = new Barrier(2);
+            
+            var rq = new TestQuery<int>(_ => Task.FromResult(42));
+            
+            var o1 = new BarrierObserver(1);
+            var o2 = new BarrierObserver(2);
+
+            var d1 = rq.Subscribe(o1);
+            var d2 = rq.Subscribe(o2);
+            
+            Thread.Sleep(100);
+
+            o1.Barrier.SignalAndWait();
+            Assert.IsFalse(o1.Broken); Assert.AreEqual(1, o1.ObservationCount);
+            o1.Barrier.SignalAndWait();
+
+            rq.Invalidate();
+            
+            o1.Barrier.SignalAndWait();
+            Assert.IsFalse(o1.Broken); Assert.AreEqual(2, o1.ObservationCount);
+            o1.Barrier.SignalAndWait();
+            
+            rq.Invalidate();
+            
+            o1.Barrier.SignalAndWait();
+            Assert.IsFalse(o1.Broken); Assert.AreEqual(3, o1.ObservationCount);
+            o1.Barrier.SignalAndWait();
+            
+            // o2 was blocked all this time, let's go check on it now
+            o2.Barrier.SignalAndWait();
+            Assert.IsFalse(o2.Broken); Assert.AreEqual(1, o2.ObservationCount);
+            o2.Barrier.SignalAndWait(); o2.Barrier.SignalAndWait();
+            Assert.IsFalse(o2.Broken); Assert.AreEqual(2, o2.ObservationCount);
+            o2.Barrier.SignalAndWait(); o2.Barrier.SignalAndWait();
+            Assert.IsFalse(o2.Broken); Assert.AreEqual(3, o2.ObservationCount);
+            o2.Barrier.SignalAndWait();
+            
+            d1.Dispose();
+            d2.Dispose();
+            
+            o1.Barrier.Dispose();
+            o2.Barrier.Dispose();
+        }
+
+        [Test]
+        public void ErrorReportingTest()
+        {
+            var ex = new Exception("Test exception");
+            var rq = new TestQuery<int>(_ => Task.FromException<int>(ex));
+            
+            var observer = new TestObserver<int>();
+            observer.TaskCompletionSource = new TaskCompletionSource<object>();
+            
+            rq.Subscribe(observer);
+            
+            Task.WaitAll(new Task[] { observer.TaskCompletionSource.Task }, 100);
+            Assert.AreEqual(new List<object>() { ex }, observer.Sequence);
+        }
+    }
+}
